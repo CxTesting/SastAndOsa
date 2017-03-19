9af0138e5ef64032df3b91a0b39eaf64b5de6d0b
cxZoharby <Zohar.BenYesha@checkmarx.com>
2017-03-19 12:17:17 +0200

    test

--- codeInjection/src/main/java/AbsolutePathTraversal.java
+++ codeInjection/src/main/java/AbsolutePathTraversal.java
@@ -1,7 +1,7 @@
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+//1
 public class AbsolutePathTraversal
 {
 	public static void doSomething(HttpServletRequest request) throws FileNotFoundException {
